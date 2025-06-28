0fe0172eb66cca73cfb5138c3784900244bf2c2e
GianLuca <cecchigl@gmail.com>
2025-06-28 08:27:48 +0200

    updating ConfigReader for external use

--- src/ConfigReader.ts
+++ src/ConfigReader.ts
@@ -3,29 +3,13 @@ import * as fs from 'fs';
 import path from 'path';
 import { Config } from './types';
 export default class ConfigReader {
-  private static instance: ConfigReader;
-  private readonly configData: Config;
+  public static config: Config;
 
-  private constructor() {
-    try {
-      this.configData = {
-        ...YAML.parse(fs.readFileSync(path.join(__dirname, '../config/config.yml'), 'utf8')),
-        ...YAML.parse(fs.readFileSync(path.join(__dirname, '../config/plugin.yml'), 'utf8'))
-      };
-    } catch (e) {
-      console.error('Error reading or parsing config.yml:', e);
-      throw e;
-    }
-  }
-
-  public static getInstance(): ConfigReader {
-    if (!ConfigReader.instance) {
-      ConfigReader.instance = new ConfigReader();
-    }
-    return ConfigReader.instance;
-  }
-
-  public get config(): Config {
-      return this.configData
+  public static generate(configPath: string, pluginPath: string): Config {
+    ConfigReader.config = {
+      ...YAML.parse(fs.readFileSync(configPath, 'utf8')),
+      ...YAML.parse(fs.readFileSync(pluginPath, 'utf8'))
+    };
+    return ConfigReader.config;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as PipelinePluginI } from './plugin/PipelinePluginI';
 export { default as PipelineStage } from './PipelineStage';
 export { default as ConfigListener } from './ConfigListener';
+export { default as ConfigReader } from './ConfigReader';
 export { start } from './startServer';
 
 export type {
